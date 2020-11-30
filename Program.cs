134b4b7a86a36a031fd3588f6100b5edb9e3940b
DEV <dev@info.local>
2020-11-30 10:47:09 -0300

    Atividade concluida

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace atv_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada do 1 ao 10");
+           
+            for (var i = 1; i <=10; i++)
+            {
+                Console.WriteLine(i);
+
+                for (var z = 1; z <=10; z++)
+                {
+                    Console.WriteLine($"{i} x {z} = {(i*z)}"); // para mostrar variavel, colocar dentro de um calculo; para calcular, dentro de parentese.
+
+                }
+            }
+           
+        }
+    }
+}
